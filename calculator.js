ba46065b9c8e011000924c4182dc0d8cdf2ee65d
dav <daviddehaas05@gmail.com>
2022-10-16 01:12:17 +0200

    Fixed the display problem and added functionality for delete button

--- calculator.js
+++ calculator.js
@@ -1,4 +1,5 @@
 const firstButtonDisplay = document.querySelector(".number-display");
+let limit = 0;
 
 function add(a,b) {
     return a + b;
@@ -33,7 +34,24 @@ function operate(a, b, operator) {
 function buttonPressed(event) {
     let button = event.target;
     let buttonText = button.textContent;
-    firstButtonDisplay.textContent += buttonText;
+    let firstDisplayText = firstButtonDisplay.textContent;
+    
+    if (button.className == "clear-button") {
+        return;
+    } else if (button.className == "delete-button") {
+        console.log("hi")
+        if (limit > 0) limit--;
+        console.log(limit)
+        firstButtonDisplay.textContent = firstDisplayText.slice(0, -1);
+        return;
+    } 
+    limit++;
+
+    if (limit < 16) {
+        firstButtonDisplay.textContent += buttonText;
+    }
+    
+
 }
 
 //this applies not to the clear and delete buttons because th
